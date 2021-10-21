6ad67c206645adca580eb19647d65b22df45150f
Shubham <shubham_landge@persistent.com>
2021-10-21 23:24:45 +0530

    logout function

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocialAuthService } from 'angularx-social-login';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(
+    private router: Router,
+    public socialAuthServive: SocialAuthService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout(): void {
+    this.socialAuthServive
+      .signOut()
+      .then(() => this.router.navigate(['login']));
   }
-
 }
